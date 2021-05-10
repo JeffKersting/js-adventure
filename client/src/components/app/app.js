3889fc89d30d7da4b2e68110e3b19a7c66ead8c4
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-05-10 17:30:51 -0600

    Add game context file

--- client/src/components/app/app.js
+++ client/src/components/app/app.js
@@ -2,12 +2,15 @@ import { useState, useEffect } from 'react'
 import Phaser from 'phaser'
 import { IonPhaser } from '@ion-phaser/react'
 import game from '../../phaser/gameConfig'
+import GameDataProvider from '../gameContext'
 
 function App() {
 
+  let [level, setLevel] = useState('SceneOne')
+
   return (
     <>
-      <IonPhaser game={game} initialize={true} className='phaser'/>
+      <IonPhaser game={game} initialize={true} className='phaser' level={level}/>
       <article className='game-border'>
         <div className='game-top'></div>
         <div className='game-sidebar'></div>
---
+++ client/src/components/gameContext.js
@@ -0,0 +1,21 @@
+import * as React from 'react'
+
+const GameContext = React.createContext({
+  level: 'SceneOne'
+})
+
+function gameReducer(state, action) {
+  switch(action.type) {
+    case 'changeLevel': {
+      return {level: state.level = action.text}
+    }
+  }
+}
+
+function GameDataProvider({children}) {
+  const [state, dispatch] = React.useReducer()
+  const value = {state, dispatch}
+  return <GameContext.Provider value={value}>{children}</GameContext.Provider>
+}
+
+export {GameDataProvider}
