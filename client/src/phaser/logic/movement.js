79786ed18e577df93c854ef37ea845eb848d8b5a
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-05-10 19:23:50 -0600

    Add physics to barrel object. Add game objective area and puzzle mechanic

--- client/src/phaser/logic/movement.js
+++ client/src/phaser/logic/movement.js
@@ -3,21 +3,23 @@ import game from '../gameConfig'
 export function moveRight(scene) {
   let player = game.scene.keys[scene].player;
   player.flipX = false;
-  player.x += 2.5;
+  player.setVelocityX(150);
 }
 
 export function moveLeft(scene) {
   let player = game.scene.keys[scene].player;
   player.flipX = true;
-  player.x -= 2.5;
+  player.setVelocityX(-150);
 }
 
 export function moveUp(scene) {
   let player = game.scene.keys[scene].player;
-  player.y -= 2.5;
+  
+  player.setVelocityY(-150);
 }
 
 export function moveDown(scene) {
   let player = game.scene.keys[scene].player;
-  player.y += 2.5;
+
+  player.setVelocityY(150);
 }
--- client/src/phaser/scenes/scene-one.js
+++ client/src/phaser/scenes/scene-one.js
@@ -13,14 +13,31 @@ class SceneOne extends Phaser.Scene {
 
 
   create() {
-    game.parent.setLevelData('Scene Two')
+    game.parent.setLevelData('if (x = 2) { return (deadlyWater) }')
     this.river = this.physics.add.group()
     this.aGrid = new AlignGrid({scene: this, cols: 30, rows: 20});
     createMap(0, 599, this, 'grass', 17)
     createObstacle(this, [22,23,52,53,82,83,112,113,142,143,172,173,202,203], 'environment', 1, this.river)
+
+    this.area = this.physics.add.sprite(100,100, 'environment', 8)
+    this.area.scale = 8
+
+    this.text = this.add.text(90,40, 'X', {fill: '#000000'})
+    this.text.scale = (2)
+
+    this.barrel = this.physics.add.sprite(100,100, 'environment', 18)
+    this.barrel.scale = 2
+    this.barrel.body.pushable = true
+    this.barrel.body.enable = true
+    this.barrel.body.drag.y = 50
+    this.barrel.body.drag.x = 50
+
+
+
+
     this.disciple = this.physics.add.sprite(400, 500, 'disciple', 0);
     this.player = this.physics.add.sprite(100, 551.5, 'characters', 35);
-    this.goal = this.physics.add.sprite(900, 100, 'environment', 50);
+    this.goal = this.physics.add.sprite(900, 100, 'environment', 48);
     this.goal.scale = 3
     this.key_A = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     this.key_D = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
@@ -28,11 +45,16 @@ class SceneOne extends Phaser.Scene {
     this.key_W = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
     this.key_P = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.P);
 
+    this.physics.add.collider(this.player, this.barrel)
 
-    this.physics.add.collider(this.player, this.disciple, function(){
-      stopCurrentScene('SceneOne');
-      startNewScene('SceneTwo');
-    })
+
+    console.log(this.barrel)
+
+
+    // this.physics.add.collider(this.player, this.disciple, function(){
+    //   stopCurrentScene('SceneOne');
+    //   startNewScene('SceneTwo');
+    // })
 
     this.physics.add.collider(this.player, this.river, () => {
       stopCurrentScene('SceneOne');
@@ -55,18 +77,22 @@ class SceneOne extends Phaser.Scene {
 
     this.input.keyboard.on('keyup_D', function(event) {
       this.player.anims.stop()
+      this.player.setVelocityX(0)
     }, this);
 
     this.input.keyboard.on('keyup_A', function(event) {
       this.player.anims.stop()
+      this.player.setVelocityX(0)
     }, this);
 
     this.input.keyboard.on('keyup_W', function(event) {
       this.player.anims.stop()
+      this.player.setVelocityY(0)
     }, this);
 
     this.input.keyboard.on('keyup_S', function(event) {
       this.player.anims.stop()
+      this.player.setVelocityY(0)
     }, this);
   }
 
@@ -98,6 +124,14 @@ class SceneOne extends Phaser.Scene {
       this.disciple.flipX = true
     } else {
       this.disciple.flipX = false
+    }
+
+    if (!this.key_A.isDown && !this.key_D.isDown) {
+      this.player.setVelocityX(0)
+    }
+
+    if (!this.key_W.isDown && !this.key_S.isDown) {
+      this.player.setVelocityY(0)
     }
   }
 }
