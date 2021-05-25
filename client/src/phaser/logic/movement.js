204ddce41f6b7dd0830aef2b40137491b8ff58c3
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-05-25 12:07:23 -0600

    Update movement speed

--- client/src/phaser/logic/movement.js
+++ client/src/phaser/logic/movement.js
@@ -3,23 +3,23 @@ import game from '../gameConfig'
 export function moveRight(scene) {
   let player = game.scene.keys[scene].player;
   player.flipX = false;
-  player.setVelocityX(150);
+  player.setVelocityX(300);
 }
 
 export function moveLeft(scene) {
   let player = game.scene.keys[scene].player;
   player.flipX = true;
-  player.setVelocityX(-150);
+  player.setVelocityX(-300);
 }
 
 export function moveUp(scene) {
   let player = game.scene.keys[scene].player;
-  
-  player.setVelocityY(-150);
+
+  player.setVelocityY(-300);
 }
 
 export function moveDown(scene) {
   let player = game.scene.keys[scene].player;
 
-  player.setVelocityY(150);
+  player.setVelocityY(300);
 }
